4f2b852923f1a93b0b0c619512e0185ea9cbf7a4
lecharles <charleslozano@gmail.com>
2016-01-19 21:50:28 -0500

    banana, cherry, three TESTS passed. back to 0 with apple. will use hint from Ilia next.

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,38 +1,52 @@
-def translate(word)
-  final_string(word)
-end
+require "pry"
 
-def check_1st_letter_vowel(word)
-  if word.chars.first == ("a" || "e" || "i" || "o" || "u")
-    first_letter_vowel = true
-  else first_letter_vowel = false
-  end
-end
-
-def grab_1st_consonant(word)
-  word[0..0] if check_1st_letter_vowel(word) == false
-end
-
+#1 convert string to array
 def split_into_array(word)
   word.split("")
+end #RETURNS USER STRING INTO ARRAY: ["c", "h", "e", "r", "r", "y"]
+
+#2 iterate through array & find first vowel in array => its index
+def find_first_vowel(array)
+  array.index { |vowel_index| ["a", "e", "i", "o", "u"].include? vowel_index }
+end #RETURNS INTEGER CORRESPONDING TO INDEX OF 1ST VOWEL: 2
+
+#3 grab all consonants before first vowel
+def grab_consonants(array)
+  i = find_first_vowel(array)
+  consonants = array[0..(i-1)]
+end #RETURNS ALL CONSONANTS BEFORE VOWEL IN AN ARRAY: ["c", "h"]
+
+#5 push these consonants to end of array
+def push_consonants(array)
+  array = array.push(grab_consonants(array))
+  # array.shift(find_first_vowel(array))
+end #RETURNS AN ARRAY WITH CONSONANTS B4 VOWELS PUSHED AT THE END.
+
+#6 push "ay" to end of string
+def add_ay(array)
+  array.push("ay")
 end
 
-def take_consonant_at_end(word)
-  new_word = split_into_array(word).push(grab_1st_consonant(word))
-  new_word.shift
-  new_word
+#7 pop the consonants from array as they have been already pushed to the end
+def pop_consonants(array)
+  i = find_first_vowel(array)
+  array.shift(i)
+  array
+end #THIS IS WHERE THE PROBLEM IS
+
+#8 convert back array to string
+def back_to_s(array)
+  array.join("")
 end
 
-def add_ay(word)
-  word_to_array = split_into_array(word)
-  word_to_array.push("ay")
-end
-
-def final_string(word)
-  if check_if_vowel(word) == true
-    add_ay(word).join("")
-  else
-    starts_with_consonant = take_consonant_at_end(word)
-    starts_with_consonant.push("ay").join("") #temporary solution
-  end
+#9 make one final latin-translation method
+def translate(word)
+  # binding.pry
+  split_array = split_into_array(word)
+  vowel_index = find_first_vowel(split_array)
+  consonants = grab_consonants(split_array)
+  new_array = push_consonants(split_array)
+  final_array = pop_consonants(new_array)
+  ay_array = add_ay(final_array)
+  translated_array = back_to_s(ay_array)
 end
